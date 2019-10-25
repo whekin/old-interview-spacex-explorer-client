67279f9a4be5dc335eba02141c76e94f45a66a94
whekin <stanislavkalishin@gmail.com>
2019-10-25 12:26:58 +0300

    Add launches page

--- src/pages/launches.js
+++ src/pages/launches.js
@@ -1,5 +1,44 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import { Header, Button, LaunchTile, Loading } from '../components';
+
+const GET_LAUNCHES = gql`
+  query LaunchesList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
+    }
+  }
+`;
 
 export default function Launches() {
-  return <div />;
+  const { loading, data, error } = useQuery(GET_LAUNCHES);
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+
+  return (
+    <>
+      <Header />
+      { data.launches && data.launches.launches &&
+        data.launches.launches.map(launch => (
+          <LaunchTile
+            key={launch.id}
+            launch={launch} />
+        ))}
+    </>
+  );
 }
