4b71389db0c396ef546fb4d05d85b0bcc666e6c6
whekin <stanislavkalishin@gmail.com>
2019-10-25 14:14:24 +0300

    Add launch page

--- src/pages/launch.js
+++ src/pages/launch.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Loading, Header, LaunchDetail } from '../components';
+import { ActionButton } from '../containers';
 
-export default function Launch() {
-  return <div />;
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      id
+      site
+      isBooked
+      rocket {
+        id
+        name
+        type
+      }
+      mission {
+        name
+        missionPatch
+      }
+    }
+  }
+`;
+
+export default function Launch({ launchId }) {
+  const { loading, data, error } = useQuery(
+    GET_LAUNCH_DETAILS,
+    {
+      variables: {
+        launchId
+      }
+    });
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  return (
+    <>
+      <Header image={data.launch.mission.missionPatch}>
+        {data.launch.mission.name}
+      </Header>
+      <LaunchDetail { ...data.launch } />
+      <ActionButton { ...data.launch } />
+    </>
+  );
 }
+
+Launch.propTypes = {
+  launchId: PropTypes.string
+};
