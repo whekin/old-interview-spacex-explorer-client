f56d876e8a6ccc71f50721f1e28ff70b52d6d9e6
whekin <stanislavkalishin@gmail.com>
2019-11-05 17:47:49 +0300

    Update GET_CART_ITEMS for getting the data from server

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -4,28 +4,38 @@ import gql from 'graphql-tag';
 import { Header, Loading } from '../components';
 import { CartItem, BookTrips } from '../containers';
 
+// export const _GET_CART_ITEMS = gql`
+//   query GetCartItems {
+//     cartItems @client
+//   }
+// `;
+
 export const GET_CART_ITEMS = gql`
   query GetCartItems {
-    cartItems @client
+    cart {
+      launches
+    }
   }
 `;
-
 
 export default function Cart() {
   const { loading, data, error } = useQuery(GET_CART_ITEMS);
   if (loading) return <Loading />;
   if (error) return <p>error</p>;
+
+  const { cart: { launches: cartItems } } = data;
+
   return (
     <>
       <Header>My Cart</Header>
-      { !data.cartItems || !data.cartItems.length
+      { !cartItems || !cartItems.length
         ? <p data-testid="empty-message">No items in your cart</p>
         : (
           <>
-            { data.cartItems.map(launchId => (
+            { cartItems.map(launchId => (
               <CartItem key={launchId} launchId={launchId} />
             ))}
-            <BookTrips cartItems={data.cartItems} />
+            <BookTrips cartItems={cartItems} />
           </>
         )}
     </>
